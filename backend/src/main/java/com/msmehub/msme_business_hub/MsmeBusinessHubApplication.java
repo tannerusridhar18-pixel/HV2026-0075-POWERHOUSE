a874f043a7636bac39c319841780e17cbb1f580c
Sridhar Tanneru <tannerusridhar18@gmail.com>
2026-08-18 11:46:26 +0530

    Backend denpendencies and folder structure

---
+++ backend/src/main/java/com/msmehub/msme_business_hub/MsmeBusinessHubApplication.java
@@ -0,0 +1,13 @@
+package com.msmehub.msme_business_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsmeBusinessHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsmeBusinessHubApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/msmehub/msme_business_hub/MsmeBusinessHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msmehub.msme_business_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsmeBusinessHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
